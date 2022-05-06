e89c7ad3b0920f88bab8f11172308218cec45f28
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-05-05 20:59:02 -0400

    edits to js variables and password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Variables
+
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "1234567890";
@@ -7,6 +8,27 @@ var specialSym = "!@#$%^&*()_-+=|[]{};:',<>./?";
 var letters
 var numbers
 var symbols
+
+function passwordLength(){
+    letters = parseInt(window.prompt("Choose a number between 8 and 128 for password length: "));
+  
+    if(isNaN (letters)){
+      window.alert("Invaild Input! Needs to be a number. Please try again"); 
+      passwordLength();
+
+    }
+
+    // Asks user how long they want their password to be
+    if (letters<8){
+        //invaid input window alert, user must pick between these numbers
+      window.alert("Invaild Input! Password must be longer than 8 characters, Please try again"); 
+      passwordLength();
+    } else if(letters > 128){
+      window.alert("Invaild Input! Password must be shorter than 128 characters, Please try again!");
+      passwordLength();
+    }
+    return letters;
+  }
 
 
 // Get references to the #generate element
